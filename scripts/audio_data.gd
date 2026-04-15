e1a62f5613f0c9730d7a279c536f438a6dda2b60
Kevin Quillen <quillen.kevin@gmail.com>
2026-04-14 22:36:24 -0500

    feat(01-02): add audio bus layout and AudioData struct
    - 5 audio buses (Master + Drums/Bass/Vocals/Other) with FFT 2048 spectrum analyzers
    - AudioData class with energy, peak_frequency, and 7 frequency bands (0-1)

---
+++ scripts/audio_data.gd
@@ -0,0 +1,11 @@
+class_name AudioData
+extends RefCounted
+
+## Overall energy level (0.0 - 1.0), average of all bands
+var energy: float = 0.0
+## Frequency of the loudest band (Hz)
+var peak_frequency: float = 0.0
+## Per-band magnitudes (0.0 - 1.0), 7 bands:
+## [sub-bass 20-60Hz, bass 60-250Hz, low-mid 250-500Hz, mid 500-2000Hz,
+##  upper-mid 2000-4000Hz, presence 4000-6000Hz, brilliance 6000-11050Hz]
+var bands: Array[float] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
