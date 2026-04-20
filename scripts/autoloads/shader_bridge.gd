a50b833de7a071b71287cebf5c3c9a618cd0075a
Kevin Quillen <quillen.kevin@gmail.com>
2026-04-19 21:29:44 -0500

    feat(02-02): rewrite shader bridge and uniforms for single-source audio
    - Replace 16 stem-based shader uniforms with 5 band-named uniforms
    - ShaderBridge reads AudioManager.audio_data (single instance)
    - Test shader uses audio_channels, audio_bands_low, audio_energy
    - project.godot shader_globals reduced from 16 to 5 entries

--- scripts/autoloads/shader_bridge.gd
+++ scripts/autoloads/shader_bridge.gd
@@ -1,34 +1,20 @@
 extends Node
 
-## Stem prefixes matching AudioManager.STEM_NAMES order
-const STEM_PREFIXES: PackedStringArray = ["drums", "bass", "vocals", "other"]
-
-# NOTE: Global shader uniforms are already declared in project.godot [shader_globals].
+# NOTE: Global shader uniforms are declared in project.godot [shader_globals].
 # We do NOT call RenderingServer.global_shader_parameter_add() here.
 # Godot Issue #77988: uniforms MUST be in project.godot for the editor to recognize them.
 # ShaderBridge only SETS values at runtime.
 
 func _process(_delta: float) -> void:
-	var data: Array[AudioData] = AudioManager.stem_data
-	for i in range(min(data.size(), STEM_PREFIXES.size())):
-		var prefix: String = STEM_PREFIXES[i]
-		RenderingServer.global_shader_parameter_set(
-			prefix + "_energy", data[i].energy
-		)
-		RenderingServer.global_shader_parameter_set(
-			prefix + "_peak_freq", data[i].peak_frequency
-		)
-		RenderingServer.global_shader_parameter_set(
-			prefix + "_bands_low",
-			Vector4(
-				data[i].bands[0], data[i].bands[1],
-				data[i].bands[2], data[i].bands[3]
-			)
-		)
-		RenderingServer.global_shader_parameter_set(
-			prefix + "_bands_high",
-			Vector4(
-				data[i].bands[4], data[i].bands[5],
-				data[i].bands[6], 0.0
-			)
-		)
+	var data: AudioData = AudioManager.audio_data
+	if data == null:
+		return
+
+	RenderingServer.global_shader_parameter_set("audio_energy", data.energy)
+	RenderingServer.global_shader_parameter_set("audio_peak_freq", data.peak_frequency)
+	RenderingServer.global_shader_parameter_set("audio_bands_low",
+		Vector4(data.bands[0], data.bands[1], data.bands[2], data.bands[3]))
+	RenderingServer.global_shader_parameter_set("audio_bands_high",
+		Vector4(data.bands[4], data.bands[5], data.bands[6], 0.0))
+	RenderingServer.global_shader_parameter_set("audio_channels",
+		Vector4(data.grouped[0], data.grouped[1], data.grouped[2], data.grouped[3]))
