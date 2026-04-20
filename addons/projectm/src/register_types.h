93a26a7e6e8936881a1a9366a7f8db931a23f7b1
Kevin Quillen <quillen.kevin@gmail.com>
2026-04-20 10:30:52 -0500

    feat(05-01): scaffold GDExtension project with godot-cpp and projectM deps
    - SConstruct build file linking projectM and OpenGL
    - register_types.cpp with GDExtension entry point
    - projectm.gdextension for Godot plugin loading
    - godot-cpp cloned at branch 4.4

---
+++ addons/projectm/src/register_types.h
@@ -0,0 +1,11 @@
+#ifndef PROJECTM_REGISTER_TYPES_H
+#define PROJECTM_REGISTER_TYPES_H
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_projectm_module(ModuleInitializationLevel p_level);
+void uninitialize_projectm_module(ModuleInitializationLevel p_level);
+
+#endif
